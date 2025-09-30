39aa85fdba759a231b7a479006946abe79ae5cfc
vxrnu <30003819@student.Chelmsford.ac.uk>
2025-09-30 13:40:12 +0100

    added book thing

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
+#vowels = (a, e, i, o, u)
+#text = input("Enter a line of text")
+
+book = input("Type in a title of a book")
+print("The length of the title is",len(book))
+print("The books title is",str.upper(book))
+
 
