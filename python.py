16ccaa1a46c72052e87a5028caeae1cf57f329be
vxrnu <30003819@student.Chelmsford.ac.uk>
2025-09-30 12:54:06 +0000

    improvement

--- python.py
+++ python.py
@@ -2,7 +2,7 @@
 #text = input("Enter a line of text")
 
 book = input("Type in a title of a book")
-print("The length of the title is",len(book))
-print("The books title is",str.upper(book))
+print("The length of the title is: ", len(book))
+print("The books title is: ", str.upper(book))
 
 
