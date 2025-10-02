6bb81bcd3beef5e771c37644aba4763027b938d1
hxrply <jackharpley1@gmail.com>
2025-10-02 11:54:26 +0100

    Add colon

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 #vowels = (a, e, i, o, u)
 #text = input("Enter a line of text")
 
-book = input("Type in a title of a book")
+book = input("Type in a title of a book: ")
 print("The length of the title is",len(book))
 print("The books title is",str.upper(book))
 
