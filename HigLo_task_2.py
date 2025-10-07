ab5494288a22b9e73c09cd650303753250a0a42f
vxrnu <30003819@student.Chelmsford.ac.uk>
2025-10-07 13:20:13 +0000

    part way thorugh guessing the word

--- HigLo_task_2.py
+++ HigLo_task_2.py
@@ -2,4 +2,16 @@ import random
 def guess_word():
     words = ["apple", "banana", "grape", "cherry", "orange"]
     words = random.choice(words)
-    
+    guess = ""
+    guesses = 0
+    while guess != words:
+        print(words[0+1])
+        guess = input("Guess a fruit: ")
+        if guess == words:
+            print("You guessed it")
+            guesses += 1
+        else:
+            print("Try again")
+            guesses += 1
+    print(f"It took you {guesses} guesses")
+guess_word()
