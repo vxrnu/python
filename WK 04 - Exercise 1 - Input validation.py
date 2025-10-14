8c167b3aab9414dedcc658b6c5b1737b9c6a9a28
vxrnu <30003819@student.Chelmsford.ac.uk>
2025-10-14 13:09:14 +0000

    updt

--- WK 04 - Exercise 1 - Input validation.py
+++ WK 04 - Exercise 1 - Input validation.py
@@ -18,8 +18,7 @@ with open("words.txt", "r") as file:
         lines.append(line.strip())
 # read all lines into a list 
 # count the number of lines in a file
-print(len(lines))
-
+number=len(lines)
 
 # Main processing of list:
 # print all items in the list
@@ -28,20 +27,32 @@ print(len(lines))
 # print all items in the list in alphabetic order A-Z (ascending), e.g. sorted.
 ascending_lines = sorted(lines)
 print(ascending_lines)
+
 # print all items in the list in alphabetic order Z-A (descending), eg sorted and reversed.
 descending_lines = sorted(lines, reverse=True)
 print(descending_lines)
+
 # print the shortest word and its' length
 shortest_word = min(lines, key=len)
 print(f"The shortest word is {shortest_word} with a length of {len(shortest_word)}")
+
 # print the longest word and its' length
-lo
+longest_word = max(lines, key=len)
+print(f"The longest word is {longest_word} and the length of the word is {len(longest_word)}")
+
 
 # print number of items in the list, the first item and the last item of list.
+print(lines[0])
+print(lines[number - 1])
 
 # print only the unique items in the list, along with the number of times each one occurs in the list
 # print the number of unique items in the list
+with open("words.txt", "r") as file:
+    lines = file.read().split()
 
+for i in lines:
+    if i[0].isupper():
+        print(i)
 
 # Scrabble / Word finder v1:
 # in a loop ask the user for a start letter,
