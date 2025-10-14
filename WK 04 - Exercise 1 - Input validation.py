f3af4476cbbe5bddd1db31b70e6ba0d69ba60891
vxrnu <30003819@student.Chelmsford.ac.uk>
2025-10-14 11:15:18 +0000

    updated exercise

---
+++ WK 04 - Exercise 1 - Input validation.py
@@ -0,0 +1,57 @@
+'''
+WEEK 03 Exercise - Application of recent learning
+Incorporate your recent studies of Python data structures and file handling to solve the following, e.g. 
+- file handling and methods
+- strings
+- lists
+- sets and/or dictionaries
+
+along with the relevant functions/methods they offer.
+
+Paul Knighton
+'''
+# Open a word_list.txt file for reading
+with open("words.txt", "r") as file:
+# Main file reading loop:
+    lines = []
+    for line in file:
+        lines.append(line.strip())
+# read all lines into a list 
+# count the number of lines in a file
+print(len(lines))
+
+
+# Main processing of list:
+# print all items in the list
+# print the number of items in the list
+
+# print all items in the list in alphabetic order A-Z (ascending), e.g. sorted.
+ascending_lines = sorted(lines)
+print(ascending_lines)
+# print all items in the list in alphabetic order Z-A (descending), eg sorted and reversed.
+descending_lines = sorted(lines, reverse=True)
+print(descending_lines)
+# print the shortest word and its' length
+shortest_word = min(lines, key=len)
+print(f"The shortest word is {shortest_word} with a length of {len(shortest_word)}")
+# print the longest word and its' length
+lo
+
+# print number of items in the list, the first item and the last item of list.
+
+# print only the unique items in the list, along with the number of times each one occurs in the list
+# print the number of unique items in the list
+
+
+# Scrabble / Word finder v1:
+# in a loop ask the user for a start letter,
+# then print all words beginning with a specific letter, which the user inputs.
+# e.g. "a"
+# andes
+# apple
+# avalanche
+
+# Scrabble / Word finder v2:
+# Extend your Scrabble / Word finder to print all words beginning with a specific letter, which are a specific number of letters long, which the user inputs.
+# e.g. inputs of "e" and "6" would find.
+# echoes
